e5c51e79370575cfe6adee82c2522c4d81a744d8
ruslanab84 <rusikabdulov@gmail.com>
2023-11-17 15:12:22 +0400

    second commit

---
+++ fraud/src/main/java/com/ruslan/fraud/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.ruslan.fraud;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
---
+++ fraud/src/main/java/com/ruslan/fraud/FraudController.java
@@ -0,0 +1,21 @@
+package com.ruslan.fraud;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@Slf4j
+@RequestMapping("api/v1/fraud-check")
+public class FraudController {
+
+    private final FraudCheckService fraudCheckService;
+
+    @GetMapping(path = "{customerId}")
+    public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerID){
+        boolean fraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerID);
+        log.info("fraud check request for customer{}",customerID);
+        return new FraudCheckResponse(fraudulentCustomer);
+    }
+}
