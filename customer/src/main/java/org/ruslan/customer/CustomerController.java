d6111bfa2c993a19e6a4dde251eccf03be66724a
ruslanab84 <rusikabdulov@gmail.com>
2023-12-07 17:59:16 +0400

    add database

--- customer/src/main/java/org/ruslan/customer/CustomerController.java
+++ customer/src/main/java/org/ruslan/customer/CustomerController.java
@@ -9,9 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequestMapping("api/v1/customers")
-public record CustomerController() {
+public record CustomerController(CustomerService customerService) {
     @PostMapping
     public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest){
         log.info("new customer registration {}", customerRegistrationRequest);
+        customerService.registerCustomer(customerRegistrationRequest);
     }
 }
