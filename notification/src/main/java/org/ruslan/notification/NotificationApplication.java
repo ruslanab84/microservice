809803f0e62837bd3a28c361055fc482130cfcb4
ruslanab84 <rusikabdulov@gmail.com>
2023-12-21 22:48:27 +0400

    add RabbitMQ service

--- notification/src/main/java/org/ruslan/notification/NotificationApplication.java
+++ notification/src/main/java/org/ruslan/notification/NotificationApplication.java
@@ -1,11 +1,36 @@
 package org.ruslan.notification;
 
+import org.ruslan.amgp.RabbitMQMessageProducer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "org.ruslan.notification",
+                "org.ruslan.amgp"
+        }
+
+)
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class,args);
     }
+
+//    @Bean
+//    CommandLineRunner commandLineRunner(
+//            RabbitMQMessageProducer producer,
+//            NotificationConfig notificationConfig
+//    ){
+//        return args -> {
+//            producer.publish(
+//                    new Person("Ruslan", 39),
+//                    notificationConfig.getInternalExchange(),
+//                    notificationConfig.getInternalNotificationRoutingKey()
+//            );
+//        };
+//    }
+//
+//    record Person(String name, int age){}
 }
